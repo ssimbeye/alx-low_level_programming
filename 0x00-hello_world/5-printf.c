212d4f9e14c6d967a160fdcc229e99a5d81ddbfd
Stanley Wanji Simbeye <simbeyewanjistanley@gmail.com>
2022-10-13 17:01:36 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - print the string
+ *
+ *   *
+ *
+ *    * Description - using the main function.
+ *
+ *     * this prints "Programming is like building a multiligual puzzle
+ *
+ *      * Return: 0
+ *
+ *       */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0)
+}
