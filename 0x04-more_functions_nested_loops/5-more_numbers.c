65ea10fa664492d9367282493306ee81020f942a
Stanley Wanji Simbeye <simbeyewanjistanley@gmail.com>
2022-10-21 15:18:48 -0700

    print line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -24,5 +24,3 @@ void print_most_numbers(void)
 
 	_putchar('\n');
 }
-
-
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ *
+ * @n: number of times underscore is printed
+ *
+ * Description: Can only use _putchar to print
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+
+	       _putchar('_');
+		i++;
+	}
+
+	_putchar('\n');
+}
