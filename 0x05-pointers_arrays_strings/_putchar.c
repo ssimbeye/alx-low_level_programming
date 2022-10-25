1eef3af6ad7646005a48491fb19b5e2700644808
Stanley Wanji Simbeye <simbeyewanjistanley@gmail.com>
2022-10-24 18:13:56 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar.c - writes character c to stdout
+ * @c: char to be printed
+ * Return: on success 1
+ */
+int _putchar.c(char c)
+{
+	return (write(1, &c, 1));
+}
